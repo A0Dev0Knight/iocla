b34aba675808758e56b350f4931d72623a2d8b98
A0Dev0Knight <al.dev.knight@gmail.com>
2024-04-04 09:13:52 +0300

    Am facut ex 0 si ex 1

--- laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
+++ laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
@@ -12,12 +12,20 @@ main:
     mov eax, 1
     mov ebx, 1
     cmp eax, ebx
-    je print                        ; TODO1: eax > ebx?
+    jz print                        ; TODO1: eax > ebx? - done
     ret
 
 print:
-    PRINTF32 `%s\n\x0`, myString
-                                    ; TODO2.2: afisati "Hello, World!" de N ori
-                                    ; TODO2.1: afisati "Goodbye, World!"
+    cmp ecx, 0
+    je endHello
+    PRINTF32 `%s %d\n\x0`, myString , ecx
+                                    ; TODO2.2: afisati "Hello, World!" de N ori - done
+                                    ; TODO2.1: afisati "Goodbye, World!" - done
+   
+    dec ecx
+    jmp print
+    ret
 
+endHello:
+    PRINTF32 `Goodbye, World!\n \x0`
     ret
