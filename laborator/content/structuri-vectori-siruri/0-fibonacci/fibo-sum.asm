2dcf144f50bbdd973011e9dad97c6e073e5af53a
A0Dev0Knight <al.dev.knight@gmail.com>
2024-04-26 00:15:59 +0300

    0 is under progress :)

--- laborator/content/structuri-vectori-siruri/0-fibonacci/fibo-sum.asm
+++ laborator/content/structuri-vectori-siruri/0-fibonacci/fibo-sum.asm
@@ -14,9 +14,29 @@ main:
     ; TODO: calculate the sum of first N fibonacci numbers
     ;       (f(0) = 0, f(1) = 1)
     xor eax, eax     ;store the sum in eax
+    
+    ; printf 1 ;asta pt 0
+    ; printf 1 ;asta pt 1
+    mov ebx, 0
+    mov eax, 1
 
+    mov ecx, 2
+    cmp N, 2
+    jz outus
+fibus:
+    ; conditia de iesire
+    cmp ecx, N
+    jz outus
     ; use loop instruction
+    ; il pun pe eax in stiva
+    push eax
+    ; adun la eax ce e inainte
+    add eax, ebx
+    pop ebx
+    inc ecx
+    jmp fibus
 
+outus:
     push eax
     push dword [N]
     push sum_print_format
